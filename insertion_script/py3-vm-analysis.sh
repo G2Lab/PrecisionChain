8f1c7b550306b3b27ae4e4b94d078737a296c901
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2023-09-19 11:43:49 +0500

    new vm file

---
+++ insertion_script/py3-vm-analysis.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+set -e
+CN=query_4k
+DR=/mnt/disks/multichain_data
+PT=/mnt/disks/test-data
+AT=/mnt/disks/test-data/preprocessing
+
+#SAMPLE NUMBERS
+PPL=5
+PPL_GS=5
+PPL_ES=5
+PPL_AF=5
+PPL_BC=5
+
+python buildChain.py -cn=$CN --datadir=$DR
+
+#CLINICAL
+
+python3 createStream-OMOP-Domain.py -cn=$CN --datadir=$DR -hp=$PT/mapping_vocab/CONCEPT_ANCESTOR.csv -dp=$PT/clinical/
+
+python3 insertData-OMOP-Domain.py -cn=$CN --datadir=$DR -hp=$PT/mapping_vocab/CONCEPT_ANCESTOR.csv -dp=$PT/clinical/ -np=$PPL -mf=$PT/samples/metadata.csv
+
+python3 createStream-OMOP-Person.py -cn=$CN --datadir=$DR
+
+python3 insertData-OMOP-Person.py -cn=$CN --datadir=$DR -dp=$PT/clinical/ --personPath=$PT/samples/metadata.csv -np=$PPL -mf=$PT/samples/metadata.csv
+
+#GENETIC
+python3 createStream-variants.py -cn=$CN --datadir=$DR
+
+##WGS
+python3 insertData-variant.py -cn=$CN --datadir=$DR -dp=$PT/vcf/wgs -mf=$PT/samples/metadata.csv -np=$PPL_GS -sq=WGS
+
+python3 insertData-variantPerson.py -cn=$CN --datadir=$DR -dp=$PT/vcf/wgs -mf=$PT/samples/metadata.csv -np=$PPL_GS -sq=WGS
+
+##WES
+python3 insertData-variant.py -cn=$CN --datadir=$DR -dp=$PT/vcf/wes -mf=$PT/samples/metadata.csv -np=$PPL_ES -sq=WES
+
+python3 insertData-variantPerson.py -cn=$CN --datadir=$DR -dp=$PT/vcf/wes -mf=$PT/samples/metadata.csv -np=$PPL_ES -sq=WES
+
+##AFFYMETRIX
+python3 insertData-variant.py -cn=$CN --datadir=$DR -dp=$PT/vcf/affymetrix -mf=$PT/samples/metadata.csv -np=$PPL_AF -sq=Affymetrix
+
+python3 insertData-variantPerson.py -cn=$CN --datadir=$DR -dp=$PT/vcf/affymetrix -mf=$PT/samples/metadata.csv -np=$PPL_AF -sq=Affymetrix
+
+##BEADCHIP
+python3 insertData-variant.py -cn=$CN --datadir=$DR -dp=$PT/vcf/beadchip -mf=$PT/samples/metadata.csv -np=$PPL_BC -sq=BeadChip
+
+python3 insertData-variantPerson.py -cn=$CN --datadir=$DR -dp=$PT/vcf/beadchip -mf=$PT/samples/metadata.csv -np=$PPL_BC -sq=BeadChip
+
+##Analysis
+python3 insertData-analysis.py -cn=$CN --datadir=$DR -dp=$PT/vcf/wgs -mf=$PT/samples/metadata.csv -pc=$AT/ -rf=$AT/relatedness -np=$PPL_GS
+
+##GTF
+python3 createStream-gtf.py -cn=$CN -dr=$DR
+
+python3 insertData-gtf.py -cn=$CN -dr=$DR -gp=$PT/gtf -vp=$PT/vcf/wgs -ap=$PT/annotations
